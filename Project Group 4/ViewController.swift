ed8f9140d5cb80f7326e54c352476637bcf95567
minjoungk <minjoungk@gmail.com>
2018-09-18 16:48:37 -0700

    Initial Commit

---
+++ Project Group 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project Group 4
+//
+//  Created by MinJoung Kim on 9/18/18.
+//  Copyright © 2018 The WIndow Specialists. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
